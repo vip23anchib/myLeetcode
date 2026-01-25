7bf80db9895b9cec90abe11811ad0d50b7bf3a0e
vip23anchib <barman23vipanchi@gmail.com>
2026-01-25 12:04:15 +0000

    5507 min pair removal to sort array

---
+++ 3507minPairRemovalToSort.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSorted(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) return false;
+        }
+        return true;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        int operations = 0;
+
+        while (!isSorted(nums)) {
+            int minSum = INT_MAX;
+            int idx = 0;
+
+            // find leftmost adjacent pair with minimum sum
+            for (int i = 0; i < nums.size() - 1; i++) {
+                int sum = nums[i] + nums[i + 1];
+                if (sum < minSum) {
+                    minSum = sum;
+                    idx = i;
+                }
+            }
+
+            // replace pair with their sum
+            nums[idx] = minSum;
+            nums.erase(nums.begin() + idx + 1);
+
+            operations++;
+        }
+
+        return operations;
+    }
+};
+
+/*
+Given an array nums, you can perform the following operation any number of times:
+
+    Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+    Replace the pair with their sum.
+
+Return the minimum number of operations needed to make the array non-decreasing.
+
+example:
+
+
+Input: nums = [5,2,3,1]
+
+Output: 2
+
+Explanation:
+
+    The pair (3,1) has the minimum sum of 4. After replacement, nums = [5,2,4].
+    The pair (2,4) has the minimum sum of 6. After replacement, nums = [5,6].
+
+The array nums became non-decreasing in two operations.
+
+*/
